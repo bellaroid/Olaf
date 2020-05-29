6abfd6184d8667e8bf28a2e6cf1c88f32d7d344e
Daniel Libonati <dlibonati@gmail.com>
2020-05-29 19:20:13 -0300

    [ADD] ujson. JSONRPC standard responses.

--- olaf/http.py
+++ olaf/http.py
@@ -1,16 +1,19 @@
-import json
+import ujson as json
 from werkzeug.wrappers import Request as WZRequest, Response as WZResponse
 from werkzeug.exceptions import BadRequest
 from werkzeug.wrappers.json import JSONMixin
-# from jwt import ExpiredSignatureError, InvalidSignatureError
 
 
 class Request(JSONMixin, WZRequest):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.json_module = json
 
 
 class Response(JSONMixin, WZResponse):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.json_module = json
 
 
 def dispatch(request):
@@ -22,9 +25,40 @@ def dispatch(request):
 
 
 def jsonrpc_dispatcher(request):
-    if not request.is_json:
-        response = Response(json.dumps({"message": "Invalid JSON"}))
+    response = Response(content_type="application/json", status=200)
+
+    try:
+        data = request.get_json()
+    except BadRequest:
+        response.set_data(json.dumps({
+            "id": None,
+            "error": {
+                "code": -32700,
+                "message": "Parse error"
+            },
+            "jsonrpc": "2.0"
+        }))
         return response
-    response = Response(json.dumps(
-        {"message": "This is the JSONRPC dispatcher"}))
+
+    if not set(data).issubset({"id", "method", "params", "jsonrpc"}):
+        response.set_data(json.dumps({
+            "id": None,
+            "error": {
+                "code": -32600,
+                "message": "Invalid Request"
+            },
+            "jsonrpc": "2.0"
+        }))
+        return response
+
+    if data["method"] not in []:     # TODO: List of available methods
+        response.set_data(json.dumps({
+            "id": data["id"],
+            "error": {
+                "code": -32601,
+                "message": "Method not found"
+            },
+            "jsonrpc": "2.0"
+        }))
+
     return response
