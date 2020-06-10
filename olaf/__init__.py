6a3f2682af526eeeb62c0c84bf323a13c5b1cbe1
Daniel Libonati <dlibonati@gmail.com>
2020-06-10 20:47:59 -0300

    [FIX] Dotenv Load at First

--- olaf/__init__.py
+++ olaf/__init__.py
@@ -1,8 +1,13 @@
+# Read .env file before doing anything else
+from dotenv import load_dotenv
+load_dotenv()
+
 from .db import ModelRegistry
 
 # Initialize Registry
 registry = ModelRegistry()
 
+from . import tools
 from . import security
 from . import fields
 from . import models
