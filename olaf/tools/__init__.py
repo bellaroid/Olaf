c0c53dba4e04bb238eab51050f3cb736d666e983
Daniel Libonati <dlibonati@gmail.com>
2020-10-11 22:05:14 -0300

    [IMP] Werkzeug log format

--- olaf/tools/__init__.py
+++ olaf/tools/__init__.py
@@ -3,5 +3,8 @@ from .config import config
 from .bootstrap import initialize
 from .logger import setup_logger
 
+# Configure Olaf and Werkzeug loggers
 logger = logging.getLogger("olaf")
+werkzeug_logger = logging.getLogger("werkzeug")
 setup_logger(logger)
+setup_logger(werkzeug_logger)
